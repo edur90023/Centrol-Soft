cedaf0e39624e64da36b4bbf635757f99aa4b57c
edur90023 <edur900@gmail.com>
2025-12-19 13:22:00 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,48 +1,50 @@
-// ARCHIVO: src/App.tsx
 import React from 'react';
 import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { Layout } from './components/Layout';
 import { Login } from './pages/Login';
 import { Dashboard } from './pages/Dashboard';
 import { Clients } from './pages/Clients';
-import { Emails } from './pages/Emails'; // Nuevo Import
-import { Repositories } from './pages/Repositories'; // Nuevo Import
+import { Emails } from './pages/Emails';
+import { Repositories } from './pages/Repositories';
+import { InstallPWA } from './components/InstallPWA'; // <--- IMPORTAR
 
 function App() {
   return (
-    <HashRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        
-        {/* Protected Routes */}
-        <Route path="/" element={
-          <Layout>
-            <Dashboard />
-          </Layout>
-        } />
-        
-        <Route path="/clients" element={
-          <Layout>
-            <Clients />
-          </Layout>
-        } />
+    <>
+      <InstallPWA /> {/* <--- COMPONENTE DE INSTALACIÓN AQUÍ */}
+      <HashRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          
+          {/* Protected Routes */}
+          <Route path="/" element={
+            <Layout>
+              <Dashboard />
+            </Layout>
+          } />
+          
+          <Route path="/clients" element={
+            <Layout>
+              <Clients />
+            </Layout>
+          } />
 
-        {/* Nuevas Rutas */}
-        <Route path="/emails" element={
-          <Layout>
-            <Emails />
-          </Layout>
-        } />
+          <Route path="/emails" element={
+            <Layout>
+              <Emails />
+            </Layout>
+          } />
 
-        <Route path="/repos" element={
-          <Layout>
-            <Repositories />
-          </Layout>
-        } />
+          <Route path="/repos" element={
+            <Layout>
+              <Repositories />
+            </Layout>
+          } />
 
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </HashRouter>
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </HashRouter>
+    </>
   );
 }
 
